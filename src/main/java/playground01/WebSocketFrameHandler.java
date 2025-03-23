b238e060f9e319b45fa9a1c3d88eafdfd99126bc
mungsil <thddmd2009@gmail.com>
2025-03-23 14:09:35 +0900

    feat: 수신한 메시지 브로드캐스트 #1

--- src/main/java/playground01/WebSocketFrameHandler.java
+++ src/main/java/playground01/WebSocketFrameHandler.java
@@ -1,19 +1,33 @@
 package playground01;
 
-import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
-public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+import java.util.HashSet;
+import java.util.Set;
+
+public class WebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private static final Set<Channel> channels = new HashSet<>();
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, WebSocketFrame webSocketFrame) throws Exception {
-        if (webSocketFrame instanceof TextWebSocketFrame textFrame) {
-            String text = textFrame.text();
-            System.out.println(text);
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        channels.add(ctx.channel());
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+        String text = textWebSocketFrame.text();
+        System.out.println("Received: "+ text);
+
+        broadcastMessage(text);
+    }
+
+    private void broadcastMessage(String text) {
+        for (Channel channel : channels) {
+            channel.writeAndFlush(new TextWebSocketFrame(text));
         }
     }
 
